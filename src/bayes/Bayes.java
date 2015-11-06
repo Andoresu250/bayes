4a09f1b62fac79ae0cab7807d6cdd637fec92c69
Andres Felipe Vera Arenas <andres-vera95@hotmail.com>
2015-11-06 11:32:32 -0500

    first commit

---
+++ src/bayes/Bayes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bayes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dell
+ */
+public class Bayes {
+
+    private ArrayList<Columna> columnas = new ArrayList<Columna>();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/bayes/Columna.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bayes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dell
+ */
+public class Columna {
+    public String name;
+    public ArrayList<String> datos = new ArrayList<>();
+
+    public Columna(String name) {
+        this.name = name;
+    }
+}
